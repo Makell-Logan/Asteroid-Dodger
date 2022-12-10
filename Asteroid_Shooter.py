c2cae777333dc452604d8ed7601181fbd0d15d08
Makell-Logan <logan01@allegheny.edu>
2022-12-10 18:21:05 -0500

    Update: Added code to Asteroid_Shooter.py to create laser class

--- Asteroid_Shooter.py
+++ Asteroid_Shooter.py
@@ -3,7 +3,7 @@ import sys
 import random
 
 class SpaceShip(pygame.sprite.Sprite):
-    def __init__(self,path,x_pos,y_pos,speed):
+    def __init__(self,path,x_pos,y_pos):
         super().__init__()
         self.image = pygame.image.load(path)
         self.rect = self.image.get_rect(center = (x_pos,y_pos))
@@ -29,35 +29,42 @@ class Meteor(pygame.sprite.Sprite):
     def update(self):
         self.rect.centerx += self.x_speed
         self.rect.centery += self.y_speed
-        if self.rect.center >= 800:
+        if self.rect.centery >= 800:
             self.kill() # This despawns the sprite once it leaves the screen
 
 class Laser(pygame.sprite.Sprite):
-        def __init__(self,path,x_pos,y_pos,speed):
+        def __init__(self,path,pos,speed):
             super().__init__()
             self.image = pygame.image.load(path)
-            self.rect = self.image.get_rect(center = (x_pos,y_pos))
-
+            self.rect = self.image.get_rect(center = (pos))
+            self.speed = speed
+        
+        def update(self):
+            self.rect.centery -= self.speed
+            if self.rect.centery <= -100:
+                self.kill()
 
 
 pygame.init() # initiate pygame
 screen = pygame.display.set_mode((1280,720)) # Create display surface
 clock = pygame.time.Clock() # Create clock object
 
-spaceship = SpaceShip("Meteor Dodger Assets/spaceship.png",640,500,10)
+spaceship = SpaceShip("Meteor Dodger Assets/spaceship.png",640,500)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
 
 meteor_group = pygame.sprite.Group()
-
 METEOR_EVENT = pygame.USEREVENT
 pygame.time.set_timer(METEOR_EVENT,100) # This event triggers every 100 miliseconds
+
+laser_group = pygame.sprite.Group()
 
 while True: # Game loop
     for event in pygame.event.get(): # Check for events / Player input
         if event.type == pygame.QUIT: # Close the game
             pygame.quit()
             sys.exit()
+
         if event.type == METEOR_EVENT:
             meteor_path = random.choice(("Meteor Dodger Assets/Meteor1.png", "Meteor Dodger Assets/Meteor2.png", "Meteor Dodger Assets/Meteor3.png"))
             random_x_pos = random.randrange(0,1280)
@@ -67,11 +74,19 @@ while True: # Game loop
             meteor = Meteor(meteor_path, random_x_pos, random_y_pos, random_x_speed, random_y_speed)
             meteor_group.add(meteor)
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            new_laser = Laser("Meteor Dodger Assets/Laser.png",event.pos,15)
+            laser_group.add(new_laser)
+
     screen.fill((42,45,51))
-    spaceship_group.draw(screen) # Adds the object to the screen
-    spaceship_group.update()
     
-    meteor_group.draw(screen) # Adds the object to the screen
+    laser_group.draw(screen)
+    spaceship_group.draw(screen) # Adds the spacesship_group object to the screen
+    meteor_group.draw(screen) # Adds the meteor_group object to the screen
+    
+
+    laser_group.update()
+    spaceship_group.update()
     meteor_group.update()
 
     pygame.display.update() # Draw frame
